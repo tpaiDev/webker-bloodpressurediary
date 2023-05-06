2b9d8ef73539039a70c549396a4dcf1858527f4d
Tapai David Zsolt <h985791@stud.u-szeged.hu>
2023-05-06 20:29:10 +0200

    Update BP-measurement component UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+
+
 import { BpmeasurementAddEditComponent } from './bpmeasurement-add-edit/bpmeasurement-add-edit.component';
+
+
 
 @NgModule({
   declarations: [
@@ -23,6 +33,12 @@ import { BpmeasurementAddEditComponent } from './bpmeasurement-add-edit/bpmeasur
     MatIconModule,
     MatButtonModule,
     MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/bpmeasurement-add-edit/bpmeasurement-add-edit.component.ts
+++ src/app/bpmeasurement-add-edit/bpmeasurement-add-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-bpmeasurement-add-edit',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./bpmeasurement-add-edit.component.scss']
 })
 export class BpmeasurementAddEditComponent {
+  bpmForm!: FormGroup;
 
+  constructor(private _fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.bpmForm = this._fb.group({
+      date: '',
+      potd: '',
+      systol: '',
+      diastol: '',
+      pulse: '',
+      comment: '',
+    })
+  }
+
+onFormSubmit() {
+  if(this.bpmForm.valid) {
+    console.log(this.bpmForm.value);
+  }
+}
+
+
+  potd: string[] = [ // parts of the day
+    'Reggel',
+    'Dél',
+    'Délután',
+    'Este',
+  ];
 }
